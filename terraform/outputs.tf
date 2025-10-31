417dc5d9bda91e762e7bc12d5db790319397063f
Chris Lindeman <slimhindrance@gmail.com>
2025-10-31 15:15:42 -0400

    Fix Terraform outputs - remove Cognito references

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,20 +5,7 @@ output "application_url" {
   value       = module.ecs.load_balancer_dns_name
 }
 
-output "cognito_user_pool_id" {
-  description = "Cognito User Pool ID"
-  value       = module.cognito.user_pool_id
-}
-
-output "cognito_user_pool_client_id" {
-  description = "Cognito User Pool Client ID"
-  value       = module.cognito.user_pool_client_id
-}
-
-output "cognito_user_pool_domain" {
-  description = "Cognito User Pool Domain"
-  value       = module.cognito.user_pool_domain
-}
+# Cognito outputs removed - now using simple JWT authentication
 
 output "s3_bucket_name" {
   description = "S3 bucket name for Canvas data"
@@ -40,8 +27,6 @@ output "frontend_config" {
   description = "Configuration object for frontend application"
   value = {
     aws_region              = var.aws_region
-    cognito_user_pool_id    = module.cognito.user_pool_id
-    cognito_user_pool_client_id = module.cognito.user_pool_client_id
     application_url         = "http://${module.ecs.load_balancer_dns_name}"
     s3_bucket_name         = module.s3.bucket_name
   }
